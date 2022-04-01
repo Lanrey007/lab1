e025aa439362b89e5b222b433b4588d180569071
Abdurrahmon Badmus <abdurrahmonbadmus.ab@gmail.com>
2022-04-01 11:47:47 +0100

    initial

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String myName = "Abdurrahmon Badmus";
+            dynamic myTrack = "Backend";
+            var myReason = "I am learning backend development because i want to add it to my knowledge of front-end so i can become a full-stack developer.";
+            Console.WriteLine("Hello " + myName +", I am in " +myTrack + " of Wella Schools.");
+            Console.WriteLine(myReason);
         }
     }
 }
